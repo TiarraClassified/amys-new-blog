ccd0682117eca5220467c640aed7b5be1aebe18b
Wheatley <tiarra.stout@gmail.com>
2018-05-31 16:21:07 -0600

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Dagger from "../dagger.jpg";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
+    <div id="navbar">
+      <Link to="/about" className="nav-link">
+        <div>About</div>
+      </Link>
+
       <img
         src={Dagger}
         alt="dagger"
         style={{ height: "50px", width: "50px" }}
       />
+      <Link to="/storyroulette" className="nav-link">
+        <div>Random Story</div>
+      </Link>
     </div>
   );
 }
