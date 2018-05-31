b422514c64e8a679fccc4087a74a7acfd54be480
Wheatley <tiarra.stout@gmail.com>
2018-05-31 17:37:40 -0600

    added title transition

--- src/components/wee-components/Title.js
+++ src/components/wee-components/Title.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Title() {
-  return (
-    <div>
-      <p id="title">
-        Micro Mini <br /> Short Stories
-      </p>
-    </div>
-  );
+export default class Title extends Component {
+  constructor() {
+    super();
+    this.state = {
+      enter: false
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ enter: true });
+    }, 10);
+  }
+  render() {
+    return (
+      <div>
+        <p
+          id="title"
+          style={
+            this.state.enter
+              ? {
+                  transform: "translate(0, 0) rotate(0)",
+                  opacity: 1,
+                  transition: "2s"
+                }
+              : {
+                  transform: "translate(-150px, -250px) rotate(-35deg)",
+                  opacity: 0
+                }
+          }
+        >
+          Micro Mini <br /> Short Stories
+        </p>
+      </div>
+    );
+  }
 }
+
+//Might have to make this a stateful component.
+//have component start at angle and invisible. once it loads, have it fall into place. setTimeout to add class that has the animation.
